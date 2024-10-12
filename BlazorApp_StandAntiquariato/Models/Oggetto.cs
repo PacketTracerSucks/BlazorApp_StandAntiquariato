649607306dad797ae8a06c402e0638c6a8e59fc9
PacketTracerSucks <leddalorenzo@hotmail.com>
2024-10-13 01:21:56 +0200

    Aggiungere i file di progetto.

---
+++ BlazorApp_StandAntiquariato/Models/Oggetto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp_StandAntiquariato.Models
+{
+    public class Oggetto
+    {
+        public int IdOggetto { get; set; }
+        public string? Tipo { get; set; }
+        public string? MaterialePrincipale { get; set; }
+        public string? Epoca { get; set; }
+        public decimal PrezzoEsposto { get; set; }
+        public int StandId { get; set; }
+        public string? PercorsoImmagine { get; set; }
+    }
+}
---
+++ BlazorApp_StandAntiquariato/Models/Stand.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp_StandAntiquariato.Models
+{
+    public class Stand
+    {
+        public int IdStand { get; set; }
+        public string? NomeAntiquario { get; set; }
+        public string? CognomeAntiquario { get; set; }
+        public char Zona { get; set; }
+        public int Superficie { get; set; }
+        public bool IsCoperto { get; set; }
+        public DateTime DataInizio { get; set; }
+    }
+}
